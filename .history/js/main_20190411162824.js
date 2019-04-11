35d66612c66023475824fd1ffba45bb8905d1456
kojimayu <bousouko777@gmail.com>
2019-04-11 16:34:42 +0900

    10end

--- .history/js/main_20190411162824.js
+++ .history/js/main_20190411162824.js
@@ -6,9 +6,5 @@
 
   a.splice(2, 0, 6, 7);
   console.log(a);
-
-  const removed = a.splice(1, 2);
-  console.log(a);
-  console.log(removed);
-
+  
 }
--- .history/js/main_20190411163101.js
+++ .history/js/main_20190411163101.js
@@ -7,7 +7,7 @@
   a.splice(2, 0, 6, 7);
   console.log(a);
 
-  const removed = a.splice(1, 2);
+  a.splice(1, 2);
   console.log(a);
   console.log(removed);
 
